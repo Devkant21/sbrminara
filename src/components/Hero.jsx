b3a6b49a5a0cb946e40c6a132b02ecee12e3c4a3
Devkant <devkant21@gmail.com>
2023-06-15 12:56:50 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Hero = () => {
-  return (
-    <div className='p-6 my-5'>
-        Hero
-    </div>
-  )
+    return (
+        <div className='p-6 my-5'>
+            <Image
+                src="/SM1.jpg"
+                width={300}
+                height={150}
+                alt='SBR'
+            />
+
+        </div>
+    )
 }
 
 export default Hero
