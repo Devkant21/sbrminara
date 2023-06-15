0b7208c7c58a4065b63b58ab9a00f9c3cd003e8a
Devkant <devkant21@gmail.com>
2023-06-15 13:08:24 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,21 @@ import Image from 'next/image'
 
 const Navbar = () => {
     return (
-        <div className='p-5 text-xl'>
+        <div className=' pt-2 text-xl'>
+            <ul className='flex justify-start'>
+                <li>
+                    <Image
+                        src="/sbr.png"
+                        width={75}
+                        height={50}
+                        alt='SBR logo'
+                    />
+                </li>
+            </ul>
             <ul className='flex justify-center space-x-5'>
-                <ul>
-                    <li>
-                        <Image
-                            src="/sbr.png"
-                            width={100}
-                            height={50}
-                            alt='SBR logo'
-                        />
-                    </li>
-                </ul>
-                <li className='cursor-pointer bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...'>Home</li>
-                <li className='cursor-pointer bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...'>Service</li>
-                <li className='cursor-pointer bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...'>Contact</li>
+                <li className='cursor-pointer rounded-md p-2 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300'>Home</li>
+                <li className='cursor-pointer rounded-md p-2 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300'>Service</li>
+                <li className='cursor-pointer rounded-md p-2 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300'>Contact</li>
             </ul>
         </div>
     )
