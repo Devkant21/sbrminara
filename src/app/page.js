4f4b420da7c2c43eae55549ac3d1caad6c6352a8
Devkant <devkant21@gmail.com>
2023-06-15 14:58:14 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -74,7 +74,7 @@ function Page() {
         {theme === 'dark' ? sun : moon}
       </button>
       <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
-        <div className="max-w-5xl w-11/12 mx-auto">
+        <div className="w-11/12 mx-auto">
           <Navbar />
           <Hero />
           <Carousel />
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,38 +1,96 @@
 import React from 'react'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const Cards = () => {
+    const [info, setInfo] = useState("0")
+
+
     return (
-        <div className='col-span-4'>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="grid grid-cols-4 gap-4 p-4 m-4">
+            <div className="card rounded-xl">
                 <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">Exterior</div>
-                    <p className="text-white text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                <h3>Restaurant Name</h3>
+                <h4>Biryani, Kebab</h4>
+                <h4>$50 for two</h4>
+            </div>
+            <div className="card rounded-xl">
+                <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
+                <h3>Restaurant Name</h3>
+                <h4>Biryani, Kebab</h4>
+                <h4>$50 for two</h4>
+            </div>
+            <div className="card rounded-xl">
+                <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
+                <h3>Restaurant Name</h3>
+                <h4>Biryani, Kebab</h4>
+                <h4>$50 for two</h4>
+            </div>
+            <div className="card rounded-xl">
+                <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
+                <h3>Restaurant Name</h3>
+                <h4>Biryani, Kebab</h4>
+                <h4>$50 for two</h4>
+            </div>
+            <div className="card rounded-xl">
+                <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
+                <h3>Restaurant Name</h3>
+                <h4>Biryani, Kebab</h4>
+                <h4>$50 for two</h4>
+            </div>
+            <div className="card rounded-xl">
+                <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
+                <h3>Restaurant Name</h3>
+                <h4>Biryani, Kebab</h4>
+                <h4>$50 for two</h4>
+            </div>
+            <div className="card rounded-xl">
+                <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
+                <div className="p-4 m-5">
+                    <h3>Restaurant Name</h3>
+                    <h4>Biryani, Kebab</h4>
+                    <h4>$50 for two</h4>
                 </div>
             </div>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="card rounded-xl">
                 <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">Exterior</div>
-                    <p className="text-white text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
+                <h3>Restaurant Name</h3>
+                <h4>Biryani, Kebab</h4>
+                <h4>$50 for two</h4>
             </div>
+
+
         </div>
+        // <div className='col-span-4'>
+        //     <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        //         <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
+        //         <div className="px-6 py-4">
+        //             <div className="font-bold text-xl mb-2">Exterior</div>
+        //             <p className="text-white text-base">
+        //                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        //             </p>
+        //         </div>
+        //         <div className="px-6 pt-4 pb-2">
+        //             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        //             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        //             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        //         </div>
+        //     </div>
+        //     <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        //         <Image src="/SM1.jpg" width={2160} height={200} alt="Sunset in the mountains" />
+        //         <div className="px-6 py-4">
+        //             <div className="font-bold text-xl mb-2">Exterior</div>
+        //             <p className="text-white text-base">
+        //                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        //             </p>
+        //         </div>
+        //         <div className="px-6 pt-4 pb-2">
+        //             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        //             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        //             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
